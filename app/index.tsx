392e9f26fa85dbe82ffb4acbf740c21ff07f134a
leesuyoen <dudgk0216@naver.com>
2025-05-21 18:35:42 +0900

    chore: 크롭 라이브러리 세팅및 불필요한 파일 삭제

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>첫 페이지입니다</Text>
+    </View>
+  );
+}

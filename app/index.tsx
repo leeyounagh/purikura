fcb53f623ef3b7e1efceb15484de67ff7bcab7ec
leesuyoen <dudgk0216@naver.com>
2025-05-26 01:15:47 +0900

    refact : 불필요한 코드 삭제

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,10 @@
-import HomeContainer from '@/components/ui/home/home';
-import { router } from 'expo-router';
-import { Button, View } from 'react-native';
-
+import HomeContainer from "@/components/ui/home/home";
+import { View } from "react-native";
 
 export default function HomeScreen() {
-      const handleGoToMain = () => {
-    router.push('/main');
-  };
   return (
-   <View style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <HomeContainer />
-
-      {/* 아래는 임시 이동용 버튼 */}
-      <Button title="Go to Main" onPress={handleGoToMain} />
     </View>
   );
 }

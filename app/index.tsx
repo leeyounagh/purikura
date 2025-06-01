d70fef7b19ca3bbed498f3971d2d1cb972cd51e4
leesuyoen <dudgk0216@naver.com>
2025-06-01 17:27:18 +0900

    dosc :리드미 수정

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import HomeContainer from "@/components/ui/home/home";
 import { View } from "react-native";
 
 export default function HomeScreen() {
+
   return (
     <View style={{ flex: 1 }}>
       <HomeContainer />
